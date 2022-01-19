64e8116f6c6dc31ed262320f50813177823799f5
evfloriz <ev.floriz@gmail.com>
2022-01-19 00:34:32 -0700

    Add and start setting up files

---
+++ src/Bus.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "CPU.h"
+
+class Bus {
+public:
+	Bus();
+	~Bus();
+
+public:
+	// Devices on bus
+	// CPU
+
+	// Fake RAM for now
+
+public:
+	// Bus read and write
+};
---
+++ src/CPU.h
@@ -0,0 +1,36 @@
+#pragma once
+
+class CPU {
+public:
+	CPU();
+	~CPU();
+
+public:
+	// CPU core registers
+
+	// External event functions
+
+	// Link CPU to bus
+
+	// Memory map of strings for disassembly - might not require
+
+	// Status register flags
+
+private:
+	// Helper functions to access status register
+
+	// Helper variables
+
+	// Facilitate link to bus
+
+	// Fetch data
+
+	// Instruction structure
+
+private:
+	// Addressing modes
+
+private:
+	// Opcodes
+
+};
