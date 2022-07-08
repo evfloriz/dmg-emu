3f08f7473e02085c4f1dcd303b7dbce65636d346
evfloriz <ev.floriz@gmail.com>
2022-07-08 16:49:05 -0600

    Combine platform files into one main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
-#include <SDL.h>
 
 #include "Util.h"
-
 #include "DMG.h"
 
+#ifdef VITA
+#include <psp2/kernel/processmgr.h>
+#include <SDL2/SDL.h>
+#include "debugScreen.h"
+
+#define printf psvDebugScreenPrintf
+#else
+#include <SDL.h>
+#endif
+
+// TODO: Reduce duplicate code between vita main and windows main
 class Demo {
 public:
 	Demo() {
@@ -16,28 +25,6 @@ public:
 
 		srcScreenRect = { 0, 0, DMG_WIDTH, DMG_HEIGHT };
 		destScreenRect = { x, y, DMG_WIDTH * pixelScale, DMG_HEIGHT * pixelScale };
-		
-		// TODO: Do another pass on the implementation of debug mode
-		if (debugMode == 1) {
-			windowName = "dmg-emu debug";
-			screenWidth = DEBUG_SCREEN_WIDTH;
-			screenHeight = DEBUG_SCREEN_HEIGHT;
-
-			srcScreenRect = { 0, 0, DMG_WIDTH, DMG_HEIGHT };
-			destScreenRect = { 0, 0, DMG_WIDTH * DEBUG_SCREEN_SCALE, DMG_HEIGHT * DEBUG_SCREEN_SCALE };
-
-			srcTileDataRect = { 0, 0, TILE_DATA_WIDTH, TILE_DATA_HEIGHT };
-			destTileDataRect = { DEBUG_SCREEN_WIDTH - (TILE_DATA_WIDTH * TILE_SCALE), 0, TILE_DATA_WIDTH * TILE_SCALE, TILE_DATA_HEIGHT * TILE_SCALE };
-
-			srcBackgroundRect = { 0, 0, MAP_WIDTH, MAP_HEIGHT };
-			destBackgroundRect = { 0, DEBUG_SCREEN_HEIGHT - MAP_HEIGHT, MAP_WIDTH, MAP_HEIGHT };
-
-			srcWindowRect = { 0, 0, MAP_WIDTH, MAP_HEIGHT };
-			destWindowRect = { MAP_WIDTH, DEBUG_SCREEN_HEIGHT - MAP_HEIGHT, MAP_WIDTH, MAP_HEIGHT };
-
-			srcObjectsRect = { 0, 0, MAP_WIDTH, MAP_HEIGHT };
-			destObjectsRect = { MAP_WIDTH * 2, DEBUG_SCREEN_HEIGHT - MAP_HEIGHT, MAP_WIDTH, MAP_HEIGHT };
-		}
 	}
 
 	~Demo() {
@@ -47,7 +34,7 @@ public:
 	int init() {
 		using namespace util;
 
-		if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) {
+		if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_GAMECONTROLLER) < 0) {
 			std::cout << "SDL could not initialize. SDL_Error: " << SDL_GetError() << std::endl;
 			return -1;
 		}
@@ -75,10 +62,6 @@ public:
 			return -1;
 		}
 
-		if (debugMode == 1) {
-			initDebugTextures();
-		}
-
 		audioSpec.freq = 44100;
 		audioSpec.format = AUDIO_F32SYS;
 		audioSpec.channels = 2;
@@ -97,49 +80,27 @@ public:
 
 		SDL_PauseAudioDevice(audioDevice, 0);
 
+#ifdef VITA
+		gameController = SDL_GameControllerOpen(0);
+		if (gameController == NULL) {
+			std::cout << "Game controller error. SDL_Error: " << SDL_GetError() << std::endl;
+			close();
+			return -1;
+		}
+#endif
+
 		// Start dmg
 		dmg.init();
 
 		return 0;
 	}
 
-	int initDebugTextures() {
-		using namespace util;
-
-		tileDataTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, TILE_DATA_WIDTH, TILE_DATA_HEIGHT);
-		if (tileDataTexture == NULL) {
-			std::cout << "Texture could not be created. SDL_Error: " << SDL_GetError() << std::endl;
-			close();
-			return -1;
-		}
-
-		backgroundTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, MAP_WIDTH, MAP_HEIGHT);
-		if (backgroundTexture == NULL) {
-			std::cout << "Texture could not be created. SDL_Error: " << SDL_GetError() << std::endl;
-			close();
-			return -1;
-		}
-
-		windowTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, MAP_WIDTH, MAP_HEIGHT);
-		if (windowTexture == NULL) {
-			std::cout << "Texture could not be created. SDL_Error: " << SDL_GetError() << std::endl;
-			close();
-			return -1;
-		}
-
-		objectsTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, MAP_WIDTH, MAP_HEIGHT);
-		if (objectsTexture == NULL) {
-			std::cout << "Texture could not be created. SDL_Error: " << SDL_GetError() << std::endl;
-			close();
-			return -1;
-		}
-
-		return 0;
-	}
-
 	int execute() {
 		SDL_Event e;
+
+#ifndef VITA
 		const uint8_t* keyboardState = SDL_GetKeyboardState(NULL);
+#endif
 
 		int secondCounter = 0;
 		uint64_t secondStart = 0;
@@ -154,6 +115,17 @@ public:
 				}
 			}
 
+#ifdef VITA
+			dmg.mmu.writeDirectionButton(0, !SDL_GameControllerGetButton(gameController, SDL_CONTROLLER_BUTTON_DPAD_RIGHT));
+			dmg.mmu.writeDirectionButton(1, !SDL_GameControllerGetButton(gameController, SDL_CONTROLLER_BUTTON_DPAD_LEFT));
+			dmg.mmu.writeDirectionButton(2, !SDL_GameControllerGetButton(gameController, SDL_CONTROLLER_BUTTON_DPAD_UP));
+			dmg.mmu.writeDirectionButton(3, !SDL_GameControllerGetButton(gameController, SDL_CONTROLLER_BUTTON_DPAD_DOWN));
+
+			dmg.mmu.writeActionButton(0, !SDL_GameControllerGetButton(gameController, SDL_CONTROLLER_BUTTON_B));		// A
+			dmg.mmu.writeActionButton(1, !SDL_GameControllerGetButton(gameController, SDL_CONTROLLER_BUTTON_A));		// B
+			dmg.mmu.writeActionButton(2, !SDL_GameControllerGetButton(gameController, SDL_CONTROLLER_BUTTON_BACK));		// Select
+			dmg.mmu.writeActionButton(3, !SDL_GameControllerGetButton(gameController, SDL_CONTROLLER_BUTTON_START));	// Start
+#else
 			// 0 is pressed, 1 is unpressed
 			dmg.mmu.writeDirectionButton(0, !keyboardState[SDL_SCANCODE_RIGHT]);
 			dmg.mmu.writeDirectionButton(1, !keyboardState[SDL_SCANCODE_LEFT]);
@@ -164,11 +136,7 @@ public:
 			dmg.mmu.writeActionButton(1, !keyboardState[SDL_SCANCODE_X]);		// B
 			dmg.mmu.writeActionButton(2, !keyboardState[SDL_SCANCODE_S]);		// Select
 			dmg.mmu.writeActionButton(3, !keyboardState[SDL_SCANCODE_A]);		// Start
-
-			// Only enable log capture in debug mode
-			/*if (keyboardState[SDL_SCANCODE_Q] && (util::debugMode == 1)) {
-				dmg.cpu.log_capture = !dmg.cpu.log_capture;
-			}*/
+#endif
 
 			// TODO: Clean up main loop
 
@@ -208,29 +176,13 @@ public:
 				std::cout << cappedFPS << " | " << uncappedFPS << "    \r" << std::flush;
 			}
 			
-			// Display audio debug information once a second
+			// Keep track of every second
 			secondCounter++;
 			if (secondCounter > 59) {
 				secondCounter = 0;
 
 				uint64_t secondEnd = SDL_GetPerformanceCounter();
 				float secondElapsed = (secondEnd - secondStart) / (float)SDL_GetPerformanceFrequency();
-
-				if (util::debugMode == 1) {
-					std::cout << "writes per second: " << dmg.apu.writeCounter << std::endl;
-					std::cout << "reads per second: " << dmg.apu.readCounter << std::endl;
-					std::cout << "writes dropped per second: " << dmg.apu.writesDropped << std::endl;
-					std::cout << "reads dropped per second: " << dmg.apu.readsDropped << std::endl;
-					std::cout << "average write pos to read pos distance: " << totalPosDistance / 60 << std::endl;
-					std::cout << "seconds per 60 frames: " << secondElapsed << std::endl;
-					std::cout << std::endl;
-				}
-
-				dmg.apu.writeCounter = 0;
-				dmg.apu.readCounter = 0;
-				dmg.apu.writesDropped = 0;
-				dmg.apu.readsDropped = 0;
-				totalPosDistance = 0;
 			}
 		}
 		return 0;
@@ -245,47 +197,7 @@ public:
 			destScreenRect,
 			util::DMG_WIDTH * util::DMG_HEIGHT);
 		
-		if (util::debugMode == 1) {
-			renderDebugTextures();
-		}
-		
 		SDL_RenderPresent(renderer);
-
-		return 0;
-	}
-
-	int renderDebugTextures() {
-		// Process tile data texture
-		renderTexture(
-			dmg.ppu.getTileDataBuffer(),
-			tileDataTexture,
-			srcTileDataRect,
-			destTileDataRect,
-			util::TILE_DATA_WIDTH * util::TILE_DATA_HEIGHT);
-
-		// Process background texture
-		renderTexture(
-			dmg.ppu.getBackgroundBuffer(),
-			backgroundTexture,
-			srcBackgroundRect,
-			destBackgroundRect,
-			util::MAP_WIDTH * util::MAP_HEIGHT);
-
-		// Process window texture
-		renderTexture(
-			dmg.ppu.getWindowBuffer(),
-			windowTexture,
-			srcWindowRect,
-			destWindowRect,
-			util::MAP_WIDTH * util::MAP_HEIGHT);
-
-		// Process objects texture
-		renderTexture(
-			dmg.ppu.getObjectsBuffer(),
-			objectsTexture,
-			srcObjectsRect,
-			destObjectsRect,
-			util::MAP_WIDTH * util::MAP_HEIGHT);
 
 		return 0;
 	}
@@ -324,10 +236,6 @@ public:
 			SDL_DestroyTexture(screenTexture);
 			screenTexture = nullptr;
 		}
-		
-		if (util::debugMode == 1) {
-			closeDebugTextures();
-		}
 
 		if (renderer) {
 			SDL_DestroyRenderer(renderer);
@@ -342,56 +250,30 @@ public:
 		if (audioDevice) {
 			SDL_CloseAudioDevice(audioDevice);
 		}
+
+#ifdef VITA
+		if (gameController) {
+			SDL_GameControllerClose(gameController);
+		}
+#endif
 		
 		SDL_Quit();
 	}
-
-	void closeDebugTextures() {
-		if (tileDataTexture) {
-			SDL_DestroyTexture(tileDataTexture);
-			tileDataTexture = nullptr;
-		}
-
-		if (backgroundTexture) {
-			SDL_DestroyTexture(backgroundTexture);
-			backgroundTexture = nullptr;
-		}
-
-		if (windowTexture) {
-			SDL_DestroyTexture(windowTexture);
-			windowTexture = nullptr;
-		}
-
-		if (objectsTexture) {
-			SDL_DestroyTexture(objectsTexture);
-			objectsTexture = nullptr;
-		}
-	}
-
 
 private:
 	SDL_Window* window = nullptr;
 	SDL_Renderer* renderer = nullptr;
 	SDL_Texture* screenTexture = nullptr;
-	SDL_Texture* tileDataTexture = nullptr;
-	SDL_Texture* backgroundTexture = nullptr;
-	SDL_Texture* windowTexture = nullptr;
-	SDL_Texture* objectsTexture = nullptr;
-
+	
 	SDL_Rect srcScreenRect;
 	SDL_Rect destScreenRect;
-	SDL_Rect srcTileDataRect;
-	SDL_Rect destTileDataRect;
-
-	SDL_Rect srcBackgroundRect;
-	SDL_Rect destBackgroundRect;
-	SDL_Rect srcWindowRect;
-	SDL_Rect destWindowRect;
-	SDL_Rect srcObjectsRect;
-	SDL_Rect destObjectsRect;
 
 	SDL_AudioDeviceID audioDevice;
 	SDL_AudioSpec audioSpec;
+
+#ifdef VITA
+	SDL_GameController* gameController;
+#endif
 
 	int screenWidth;
 	int screenHeight;
@@ -403,19 +285,27 @@ private:
 };
 
 int main(int argc, char **argv) {
+#ifdef VITA
+	psvDebugScreenInit();
+#endif
+
 	if (util::readOptionsFile() == -1) {
-		std::cout << "Exiting..." << std::endl;
+		printf("Exiting...\n");
+#ifdef VITA
+		sceKernelDelayThread(3 * 1000000);
+		sceKernelExitProcess(0);
+#endif
 		return 1;
 	}
 
 	Demo demo;
 
-	if (demo.init() == -1) {
-		std::cout << "DMG init failed, exiting..." << std::endl;
-		return 1;
-	};
+	demo.init();
 	demo.execute();
 	demo.close();
 
+#ifdef VITA
+	sceKernelExitProcess(0);
+#endif
     return 0;
 }
